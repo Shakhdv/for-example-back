1f75199f9892490178914db05f71e2d1ad8b9f6d
Abdurrohman47 <magomadov_47@mail.ru>
2023-11-29 16:25:16 +0300

    бэк для пользователей

---
+++ models/User.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+  name: String,
+  age: Number,
+  login: String,
+  password: String,
+  avatar: String,
+  programs: [
+    {
+      complete: {
+        type: Boolean,
+        default: false
+      },
+      program: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "Program",
+      },
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
